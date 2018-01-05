13297b22ef7cfe2717f7e742c26b90e8beb6f99f
Jerry Gamblin <jerry.gamblin@gmail.com>
2018-01-05 12:36:00 -0600

    Fixed CSV Creation
    Fixed CSV Creation

--- GithubDependencyCheck.sh
+++ GithubDependencyCheck.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 username="yourlogin@github.com"
 passwordtoken="get from here: https://github.com/settings/tokens"
-org="Your Org"
+org="$1"
 repos=$(curl -u $username:$passwordtoken -s https://api.github.com/orgs/$org/repos?per_page=200 | jq -r .[].name | sort )
+
+mkdir results
 
 for repo in $repos
 do
@@ -12,16 +14,22 @@ node=$(curl -u $username:$passwordtoken -s -o /dev/null -I -w "%{http_code}" htt
   if [ $node -eq "200" ]; then
     printf "Testing %s. \n" "$repo"
     curl -s -u $username:$passwordtoken https://raw.githubusercontent.com/$org/$repo/$defaultbranch/package.json > package.json
-    dependency-check --scan ./package.json --project "$repo" --format HTML --out $repo.html
-    #dependency-check --scan ./package.json --project "$repo" --format JSON--out $repo.json 
+    dependency-check --scan ./package.json --project "$repo" --format CSV --out results/$repo.csv
+    printf "\n\n"
   else
     ruby=$(curl -u $username:$passwordtoken -s -o /dev/null -I -w "%{http_code}" https://raw.githubusercontent.com/$org/$repo/$defaultbranch/Gemfile.lock)
     if [ $ruby -eq "200" ]; then
     printf "Testing %s. \n" "$repo"
     curl -s -u $username:$passwordtoken https://raw.githubusercontent.com/$org/$repo/$defaultbranch/Gemfile.lock > Gemfile.lock
-    dependency-check --scan ./Gemfile.lock --project "$repo" --format HTML --out $repo.html  
-    #dependency-check --scan ./Gemfile.lock --project "$repo" --format JSON --out $repo.json
+    dependency-check --scan ./Gemfile.lock ---project "$repo" --format CSV --out results/$repo.csv
+    printf "\n\n"
   fi
   printf "%s is not a Node or Ruby Project. Unable to run dependency-check. \n\n" "$repo"
   fi
 done
+
+#Consulidate The Report
+ cat results/*.csv > results/temp.csv
+ awk '!x[$0]++' results/temp.csv > results/temp2.csv
+ cut -d',' -f1-4,6- results/temp2.csv > githubvulns.csv
+ rm results/*.csv
